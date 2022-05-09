0c77849fe302189f105e93f3310e40a6ad1a85a3
vanntrong <89147472+vanntrong@users.noreply.github.com>
2022-05-10 00:07:56 +0700

    update

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const httpServer = createServer(app);
 
 //middleware
 app.use(cors());
-// app.use(morgan("combined"));
+app.use(morgan("combined"));
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cookieParser());
@@ -80,14 +80,14 @@ const getSocketId = (userId) => {
 
 io.on("connection", (socket) => {
   socket.on("setup", (userId) => {
-    console.log("User connect: ", userId);
+    // console.log("User connect: ", userId);
     addNewUser(userId, socket.id);
     socket.emit("getOnlineUsers", onlineUsers);
   });
 
   socket.on("join chat", (room) => {
     socket.join(room);
-    console.log("User joined chat: ", room);
+    // console.log("User joined chat: ", room);
   });
 
   socket.on("newMessage", (message, conversation) => {
@@ -156,7 +156,7 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnect");
+    // console.log("User disconnect");
     removeUser(socket.id);
     socket.emit("getOnlineUsers", onlineUsers);
   });
